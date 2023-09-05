ad52b73de9c5c1d83dc30362d10c03d8986177ff
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-09-05 23:56:44 +0530

    modification in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+/* eslint-disable no-unused-vars */
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [num, setNum] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +21,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
